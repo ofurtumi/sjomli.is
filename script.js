118274870192b8b92543aba5091c26af8162e3d4
ofurtumi <thorvaldur.tumi@gmail.com>
2021-05-01 10:49:48 +0000

    Update á myndum

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = choosePic;
+
+var myPix = new Array("/myndir/t1.png","/myndir/t2.png","/myndir/t3.png","/myndir/t4.png","/myndir/t5.png","/myndir/t6.png");
+
+function choosePic() {
+     var randomNum = Math.floor(Math.random() * myPix.length);
+     document.getElementById("mynd").src = myPix[randomNum];
+}
