bc874c57e606ba06e644990897827e4a35124d21
ofurtumi <60275253+ofurtumi@users.noreply.github.com>
2021-05-14 11:47:19 +0000

    Allskonar uppgrade úr vinnunni

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 window.onload = choosePic;
 
-var myPix = new Array("/myndir/t1.png","/myndir/t2.png","/myndir/t3.png","/myndir/t4.png","/myndir/t5.png","/myndir/t6.png");
+var myPix = new Array(
+    "/myndir/t1.png",
+    "/myndir/t2.png",
+    "/myndir/t3.png",
+    "/myndir/t4.png",
+    "/myndir/t5.png",
+    "/myndir/t6.png"
+    );
 
 function choosePic() {
      var randomNum = Math.floor(Math.random() * myPix.length);
