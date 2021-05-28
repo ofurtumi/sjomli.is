d270b2dfb2291fa0cdc52c3747718d6799199d15
ofurtumi <thorvaldur.tumi@gmail.com>
2021-05-28 23:59:54 +0000

    heill hellingur

--- blogg/includes/public_functions.php
+++ blogg/includes/public_functions.php
@@ -14,8 +14,8 @@ function getPublishedPosts() {
     $final_posts = array();
     foreach ($posts as $post) {
         $post["topic"] = getPostTopic($post["id"]);
-        #array_unshift($final_posts, $post);
-        array_push($final_posts, $post);
+        array_unshift($final_posts, $post);
+        #array_push($final_posts, $post);
     }
 
 	return $final_posts;
