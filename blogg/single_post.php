46ea33d1370aba02057db32ac982e5f09e2dde74
ofurtumi <60275253+ofurtumi@users.noreply.github.com>
2021-06-01 10:49:02 +0000

    gamalt úrelt

--- blogg/single_post.php
+++ blogg/single_post.php
@@ -9,40 +9,25 @@ $PageTitle = $post["title"];
 include("../header.php");
 ?>
 
-<div class="container">
-<div class="content" >
-		<div class="post-wrapper">
-			<div class="full-post-div">
-			<?php if ($post['published'] == false): ?>
-				<h2 class="post-title">Það er ekki búið að birta þessa færslu</h2>
-			<?php else: ?>
-				<h2 class="post-title"><?php echo $post['title']; ?></h2>
-				<div class="post-body-div">
-					<?php echo html_entity_decode($post['body']); ?>
-				</div>
-			<?php endif ?>
-			</div>
-			
-			<!-- comments section -->
-			<!--  coming soon ...  -->
-		</div>
+<div class="blogg" >
+	<div class="single-post">
+		<?php if ($post['published'] == false): ?>
+			<h2 class="post-title">Það er ekki búið að birta þessa færslu</h2>
+		<?php else: ?>
+			<h2><?php echo $post['title']; ?></h2>
+			<?php echo html_entity_decode($post['body']); ?>
+			<img src="<?php echo 'static/images/' . $post['image']; ?>">
+		<?php endif ?>
+	</div>
 
-		<!-- post sidebar -->
-		<div class="post-sidebar">
-			<div class="card">
-				<div class="card-header">
-					<h2>Topics</h2>
-				</div>
-				<div class="card-content">
-					<?php foreach ($topics as $topic): ?>
-						<a 
-							href="<?php echo BASE_URL . 'blogg/filtered_posts.php?topic=' . $topic['id'] ?>">
-							<?php echo $topic['name']; ?>
-						</a> 
-					<?php endforeach ?>
-				</div>
-			</div>
-		</div>
+	<div class="single-post-card">
+		<h2>Umræðuefni</h2>
+		<?php foreach ($topics as $topic): ?>
+			<a 
+				href="<?php echo BASE_URL . 'blogg/filtered_posts.php?topic=' . $topic['id'] ?>">
+				<?php echo $topic['name']; ?>
+			</a> 
+		<?php endforeach ?>
 	</div>
 </div>
 
