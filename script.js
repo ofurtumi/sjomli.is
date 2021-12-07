9f4e5ea8307f2aee6a33561aae86484b540932b7
Þorvaldur Tumi Baldursson <thorvaldur.tumi@gmail.com>
2021-12-07 13:22:11 +0000

    update myndir

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-window.onload = choosePic;
+// window.onload = choosePic;
 
-var myPix = new Array(
-    "/myndir/t1.png",
-    "/myndir/t2.png",
-    "/myndir/t3.png",
-    "/myndir/t4.png",
-    "/myndir/t5.png",
-    "/myndir/t6.png"
-    );
+// var myPix = new Array(
+//     "/myndir/t1.png",
+//     "/myndir/t2.png",
+//     "/myndir/t3.png",
+//     "/myndir/t4.png",
+//     "/myndir/t5.png",
+//     "/myndir/t6.png"
+//     );
 
-function choosePic() {
-     var randomNum = Math.floor(Math.random() * myPix.length);
-     document.getElementById("mynd").src = myPix[randomNum];
+// function choosePic() {
+//      var randomNum = Math.floor(Math.random() * myPix.length);
+//      document.getElementById("mynd").src = myPix[randomNum];
+// }
+
+const gleraugu = document.querySelector("#gleraugu");
+const grima = document.querySelector("#grima");
+const mynd = document.querySelector("#mynd");
+
+gleraugu.addEventListener("click",hvadErA);
+grima.addEventListener("click",hvadErA);
+
+function hvadErA() {
+    let gr = grima.checked;
+    let gl = gleraugu.checked;
+
+    if (gr && gl) mynd.setAttribute("src","./myndir/andlit2/gg.jpg");
+    else if (gr) mynd.setAttribute("src","./myndir/andlit2/egg.jpg");
+    else if (gl) mynd.setAttribute("src","./myndir/andlit2/geg.jpg");
+    else mynd.setAttribute("src","./myndir/andlit2/egeg.jpg");
 }
+
+hvadErA()
