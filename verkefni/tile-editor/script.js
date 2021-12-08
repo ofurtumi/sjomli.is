c0bb55060a04b2214481f06ce7551c1efc54faf1
Tumi <thorvaldur.tumi@gmail.com>
2021-12-08 00:29:30 +0000

    bæta við tilesettið

--- verkefni/tile-editor/script.js
+++ verkefni/tile-editor/script.js
@@ -3,16 +3,10 @@ const h = document.querySelector("#height");
 const wt = document.querySelector("#wtiles");
 const ht = document.querySelector("#htiles");
 const grid = document.querySelector("#grid");
+const check = document.querySelector("#gridcheck");
 
 let brush = "tomt";
-
-w.textContent = window.innerWidth;
-wt.textContent = finnaGrid(window.innerWidth);
-h.textContent = window.innerHeight;
-ht.textContent = finnaGrid(window.innerHeight);
-wt.addEventListener("change", (e) => {
-  console.log("breyttist");
-});
+check.checked = true;
 
 window.addEventListener("resize", (e) => {
   w.textContent = window.innerWidth;
@@ -21,16 +15,27 @@ window.addEventListener("resize", (e) => {
   ht.textContent = finnaGrid(window.innerHeight);
 });
 
-function finnaGrid(wh) {
-  let temp = wh % 64;
-  return ((wh - temp) / 64)-5;
-}
+check.addEventListener("click",(e)=> {
+  let temp = grid.querySelectorAll(".cell");
+  if (check.checked) {
+    temp.forEach(element => {
+      element.classList.add("cell-grid")
+    });
+  }
+  else {
+    temp.forEach(element => {
+      element.classList.remove("cell-grid")
+    });
+  }
+})
 
-// function tileHeight (tileFjoldi) {
-//     if (tileFjoldi !== grid.childElementCount) {
-//         grid.insertBefore()
-//     }
-// }
+function finnaGrid(wh, width = false) {
+  if (width) {
+    wh = wh - 256;
+  }
+  let temp = wh % 64;
+  return (wh - temp) / 64 - 1;
+}
 
 function empty(element) {
   if (!element || !element.firstChild) {
@@ -57,7 +62,7 @@ function el(name, ...children) {
 }
 
 async function palletteCreator() {
-  const pallette = document.querySelector(".chooser")
+  const pallette = document.querySelector(".chooser");
   tiles = await fetch("tiles.json");
   tiles = await tiles.json();
   tiles = tiles.tiles;
@@ -67,50 +72,48 @@ async function palletteCreator() {
 
     for (let i = 0; i < 3; i++) {
       let row = el("div");
-      row.classList.add("pallette-row")
+      row.classList.add("pallette-row");
 
       for (let j = 1; j <= 3; j++) {
         let c = el("div");
-        let thisTile = tiles[i*3+j+u*9]["tile"];
-        c.classList.add("pallette-cell",thisTile);
-        c.addEventListener("click",(e) => {
+        let thisTile = tiles[i * 3 + j + u * 9]["tile"];
+        c.classList.add("pallette-cell", thisTile);
+        c.addEventListener("click", (e) => {
           // console.log(brush)
           // document.querySelector("."+brush).classList.remove("valid");
           // c.classList.add("valid")
           brush = thisTile;
-          thisTile.classList
-        })
-        row.appendChild(c)
+          thisTile.classList;
+        });
+        row.appendChild(c);
       }
       block.appendChild(row);
     }
-    pallette.appendChild(block)
+    pallette.appendChild(block);
   }
 }
 
 function onstart() {
   let height = finnaGrid(window.innerHeight);
-  let width = finnaGrid(window.innerWidth);
+  let width = finnaGrid(window.innerWidth, true);
   for (let i = 0; i < height; i++) {
     let row = el("div");
     row.className = "row";
     for (let j = 0; j < width; j++) {
-        let cell = el("div");
-        cell.classList.add("cell","tomt");
-        cell.addEventListener("click",(e) => {
-            e.stopPropagation();
-            let klasi = brush;
-            let tempKlasi = cell.classList[1];
-            if (tempKlasi===klasi) {
-                cell.classList.replace(tempKlasi,"tomt")
-            }
-            else {
-                console.log("ekki tomt")
-                cell.classList.replace(tempKlasi,klasi)
-            }
-
-        })
-        row.appendChild(cell)
+      let cell = el("div");
+      cell.classList.add("cell", "tomt", "cell-grid");
+      cell.addEventListener("click", (e) => {
+        e.stopPropagation();
+        let klasi = brush;
+        let tempKlasi = cell.classList[1];
+        if (tempKlasi === klasi) {
+          cell.classList.replace(tempKlasi, "tomt");
+        } else {
+          console.log("ekki tomt");
+          cell.classList.replace(tempKlasi, klasi);
+        }
+      });
+      row.appendChild(cell);
     }
     grid.appendChild(row);
   }
