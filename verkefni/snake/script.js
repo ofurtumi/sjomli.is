42b0907918550a6bd8ef1b740711ff70d3113582
Tumi <thorvaldur.tumi@gmail.com>
2022-05-02 22:46:58 +0000

    fartölvan að verða batteryslaus, hleddari upp í skola :(

---
+++ verkefni/snake/script.js
@@ -0,0 +1,131 @@
+const canvas = document.querySelector('#snakeCanvas');
+const c = canvas.getContext('2d');
+const gravity = 1;
+const maxGrav = 15;
+const idles = new Array();
+for (let i = 1; i <= 6; i++) {
+	const img = new Image(84, 66);
+	img.src = './sprites/i' + 1 + '.png';
+	idles.push(img);
+}
+
+class Sprite {
+	constructor({ pos, vel, size = { w: 50, h: 50 }, color = 'red' }) {
+		this.pos = pos;
+		this.size = size;
+		this.vel = vel;
+		this.color = color;
+		this.RW = false; // * H veggjatékk
+		this.LW = false; // * V veggjatékk
+		this.state = 1;
+	}
+
+	draw() {
+		c.drawImage(idles[((this.state++)%6)+1], this.pos.x, this.pos.y);
+	}
+
+	update() {
+		// ! teiknar sprite með uppfærðum stöðum
+		this.draw();
+
+		// * sér um movement fyrir x ás
+		this.pos.x += this.vel.x;
+		if (this.vel.y == 0) {
+			if (this.vel.x > 0) this.vel.x -= 1; // sér um að hægja á fyrir ->
+			if (this.vel.x < 0) this.vel.x += 1; // sér um að hægja á fyrir <-
+		}
+
+		if (this.pos.x + this.size.w + this.vel.x >= canvas.width) {
+			this.vel.x = 0;
+			this.RW = true;
+			this.pos.x = canvas.width - this.size.w;
+		} else this.RW = false;
+
+		if (this.pos.x + this.vel.x <= 0) {
+			this.vel.x = 0;
+			this.LW = true;
+			this.pos.x = 0;
+		} else this.LW = false;
+
+		// * sér um movement fyrir y ás
+		// ! mjög mikið WIP
+		this.pos.y += this.vel.y;
+		if (this.pos.y + this.size.h >= canvas.height) {
+			this.vel.y = 0;
+			this.grounded = true;
+			this.pos.y = canvas.height - this.size.h;
+		} else if (this.vel.y < maxGrav) {
+			this.vel.y += gravity;
+			this.grounded = false;
+		} else {
+			this.vel.y = maxGrav;
+			this.grounded = false;
+		}
+	}
+}
+
+const keys = {
+	right: false,
+	left: false,
+	up: false,
+};
+
+const player = new Sprite({
+	pos: {
+		x: 10,
+		y: 10,
+	},
+	size: {
+		w: 84,
+		h: 66,
+	},
+	vel: {
+		x: 0,
+		y: 0,
+	},
+});
+
+function animate() {
+	window.requestAnimationFrame(animate);
+	c.fillStyle = '#000';
+	c.fillRect(0, 0, canvas.width, canvas.height);
+	player.update();
+	if (keys.right && player.vel.x < 20 && !player.RW && player.grounded)
+		player.vel.x += 2;
+	if (keys.left && player.vel.x > -20 && !player.LW && player.grounded)
+		player.vel.x -= 2;
+	if (keys.up && player.grounded) player.vel.y = -20;
+	// player.nextFrame();
+}
+
+window.addEventListener('keydown', (e) => {
+	switch (e.key) {
+		case 'ArrowRight':
+			keys.right = true;
+			break;
+		case 'ArrowLeft':
+			keys.left = true;
+			break;
+		case 'ArrowUp':
+			keys.up = true;
+			break;
+		default:
+			break;
+	}
+});
+
+window.addEventListener('keyup', (e) => {
+	switch (e.key) {
+		case 'ArrowRight':
+			keys.right = false;
+			break;
+		case 'ArrowLeft':
+			keys.left = false;
+		case 'ArrowUp':
+			keys.up = false;
+		default:
+			break;
+	}
+});
+
+animate();
