b8cc813b7efd890d92c2d2db6ede172ba87be7f7
ofurtumi <thorvaldur.tumi@gmail.com>
2022-09-22 12:00:36 +0000

    n stórar fleytitölur komnar, ath á eftir að testa nákvæmlega þannig notið á eigin ábyrgð

--- floating/script.js
+++ floating/script.js
@@ -11,15 +11,45 @@ const butt = document.querySelector('#getRes');
 const inp = document.querySelector('#floatIn');
 const output = document.querySelector('#decOut');
 
-inp.addEventListener("keypress", (e) => {
-	if(e.key === 'Enter') {
-		console.log('e.key --> ', e.key)
-		getRes.click();
-	}
+const lenInp = document.querySelector('#len');
+const expoInp = document.querySelector('#expo');
+const changer = document.querySelector('#change');
+
+changer.addEventListener('click', () => {
+	setGlobals(Number(lenInp.value), Number(expoInp.value));
 })
 
+let blen; // ? hver er heildarlengd fleytitölunar
+let bias; // ? hver er bias fyrir tölu af stærð blen
+let elen; // ? hver er lengd expo bitastrengsins
+
+/**
+ * fall til þess að auðvelda frumstillingar á breytum
+ * @param {Number} bl bit length, heildarlengd fleytitölunar í bitum (bl >= 3)
+ * @param {Number} el exponent length, stærð expo bitastrengs (el <= bl -2)
+ */
+function setGlobals(bl, el) {
+	// * bias reiknaður með stærð 2^(expo í bitum-1)-1
+	// ? fyrir 3ja bita expo er það (2^2)-1 þ.e. maður dregur 3 frá expo
+	bias = Math.pow(2, el - 1) - 1;
+	blen = bl;
+	elen = el;
+}
+
+setGlobals(6, 3);
+
+inp.addEventListener('keypress', (e) => {
+	if (e.key === 'Enter') {
+		console.log('e.key --> ', e.key);
+		getRes.click();
+	}
+});
+
 getRes.addEventListener('click', (e) => {
+	// * útbýr listann whole úr streng og breytir gildum yfir í Number
 	let whole = inp.value.split('').map((x) => Number(x));
+
+	// * athugar hvort stak í fallinu sé utann 1 eða 0
 	let works = true;
 	for (const num of whole) {
 		if (num === 1 || num === 0) {
@@ -30,32 +60,48 @@ getRes.addEventListener('click', (e) => {
 		}
 	}
 
-	// * bias reiknaður með stærð 2^(expo í bitum-1)-1
-	// ? fyrir 3ja bita expo er það (2^2)-1 þ.e. maður dregur 3 frá expo
-	if (whole.length == 6 && works) {
+	if (whole.length == blen && works) {
 		output.classList.remove('rautt');
+		// let eCheck = ;
 
-		if (whole[1] + whole[2] + whole[3] === 3)
-			output.textContent =
-				whole[4] + whole[5] === 0
-					? 'Infinity'
-					: 'NaN (þetta er ekki villa)';
-		else {
+		// ? athugar hvort sé out of bounds eða infinity
+		if (whole.slice(1, 1 + elen).reduce((a, b) => a + b, 0) === bias) {
+			output.textContent = '';
+
+			if (whole[0] === 1) {
+				output.textContent = '-';
+			}
+
+			if (whole.slice(1 + elen).reduce((a, b) => a + b, 0) === 0) {
+				output.textContent += 'Infinity';
+			} else {
+				output.textContent += 'NaN (þetta er ekki villa)';
+			}
+		} else {
 			let minus = whole[0];
-			let expo = whole[1] * 4 + whole[2] * 2 + whole[3] - 3;
-			console.log('expo --> ', expo);
-			let stable = expo > -3;
+			// ? sér til að sé aðeins verið að vinna með veldisbitana
+			// ? framkvæmir einfalda bitasamlagninu til að finna exponent
+			let expo = whole
+				.slice(1, 1 + elen)
+				.reduce((a, b, c) => a + b * Math.pow(2, elen - (c + 1)), 0);
+			expo -= bias;
+
+			let stable = expo > -1 * bias;
 
 			let dec = stable
-				? Math.pow(2, expo) +
-				  whole[4] * Math.pow(2, expo - 1) +
-				  whole[5] * Math.pow(2, expo - 2)
-				: whole[4] * Math.pow(2, expo) +
-				  whole[5] * Math.pow(2, expo - 1);
+				? whole
+						.slice(1 + elen)
+						.reduce(
+							(a, b, c) => a + b * Math.pow(2, expo - (c + 1)),
+							Math.pow(2, expo)
+						)
+				: whole
+						.slice(1 + elen)
+						.reduce((a, b, c) => a + b * Math.pow(2, expo - c), 0);
 			output.textContent = Math.pow(-1, minus) * dec;
 		}
 	} else {
 		output.classList.add('rautt');
-		output.textContent = 'tala verður að vera gild 6-bita tala';
+		output.textContent = 'tala verður að vera gild ' + blen + ' bita tala';
 	}
 });
